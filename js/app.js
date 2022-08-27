504038d613692e9a81371328bfb344f8ec4be2be
raselASrock <mdraselm413@gmail.com>
2022-08-27 19:14:06 +0600

    user-id-added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-console.log('App.js')
+function loadData2(){
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then( res => res.json())
+    .then( data => console.log(data));
+}
+
+function loadUsers (){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then( res => res.json())
+    // .then( data => console.log(data))
+    .then( data => displayUsers(data))
+}
+
+function displayUsers (data){
+    console.log(data);
+}

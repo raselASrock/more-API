acd09389e8bff127ea2933685b50f130a4d9ba1f
raselASrock <mdraselm413@gmail.com>
2022-08-27 23:02:44 +0600

    final

--- js/index.js
+++ js/index.js
@@ -20,3 +20,39 @@ function displayUsers2(data){
             ul.appendChild(li);
         }
     }
+
+    function deletPost() {
+        fetch('https://jsonplaceholder.typicode.com/posts/1', {
+            method: 'DELETE',
+        });
+    }
+    
+    function patchApost() {
+        fetch('https://jsonplaceholder.typicode.com/posts/1', {
+            method: 'PATCH',
+            body: JSON.stringify({
+                title: 'foo',
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+            .then((response) => response.json())
+            .then((json) => console.log(json));
+    }
+    
+    function createAPost(){
+        fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      body: JSON.stringify({
+        title: 'foo',
+        body: 'bar',
+        userId: 1,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+    }
--- js/post.js
+++ js/post.js
@@ -31,22 +31,3 @@ function displayPosts(posts) {
 }
 
 
-function deletPost() {
-    fetch('https://jsonplaceholder.typicode.com/posts/1', {
-        method: 'DELETE',
-    });
-}
-
-function patchApost() {
-    fetch('https://jsonplaceholder.typicode.com/posts/1', {
-        method: 'PATCH',
-        body: JSON.stringify({
-            title: 'foo',
-        }),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-        },
-    })
-        .then((response) => response.json())
-        .then((json) => console.log(json));
-}
